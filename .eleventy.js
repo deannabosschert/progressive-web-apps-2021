66c261c07fa3eb269d178d2e7bcb77d45ef3e4b0
Deanna Bosschert <36165810+deannabosschert@users.noreply.github.com>
2021-03-26 17:56:54 +0100

    feat: add ServiceWorker plugin
    apparently there's a plugin for this already lmao

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const pluginPWA = require("eleventy-plugin-pwa")
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('assets/fonts')
   eleventyConfig.addPassthroughCopy('assets/img')
+  eleventyConfig.addPlugin(pluginPWA);
 
+  
   return {
     dir: {
       input: 'src',
